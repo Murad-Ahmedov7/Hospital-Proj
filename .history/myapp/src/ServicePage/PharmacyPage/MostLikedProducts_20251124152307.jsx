3e29d238e655c71b931c2aaefdcba34134e1a157
Murad Ahmedov <mrdhmdv04@gmail.com>
2025-11-24 17:03:13 +0400

    some navbar,consultation and pharmacy responsivity updated

---
+++ .history/myapp/src/ServicePage/PharmacyPage/MostLikedProducts_20251124152307.jsx
@@ -0,0 +1,35 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination"; 
+import Card from "../../components/Card";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
+
+export default function MostLikedProducts({ slides }) {
+  return (
+    <div className="flex flex-col justify-center items-center h-[40%] gap-7 ">
+      <p className="text-gray-600 text-2xl font-bold">Top Choices</p>
+      <p className="font-bold text-5xl">Our Most-liked Products</p>
+
+      {/* 🌀 SWIPER START */}
+      <Swiper
+        modules={[Navigation, Autoplay, Pagination]}
+        slidesPerView={3} // eyni anda 3 məhsul göstərir
+        navigation
+        loop
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        className="w-[40%] flex justify-center items-center relative"
+      >
+        {slides.map((s, i) => (
+          <SwiperSlide key={i}>
+            <Card s={s} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* 🌀 SWIPER END */}
+    </div>
+  );
+}
